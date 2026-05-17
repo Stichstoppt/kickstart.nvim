4c33a959a278aa63c2af2fa5f6e9e620b5d5b0c6
Paul Stych <paul.stych@icloud.com>
2026-05-17 23:32:34 +0200

    changed theme to retrobox

--- init.lua
+++ init.lua
@@ -383,18 +383,24 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
-  }
+  -- KEIN vim.pack.add nötig, da retrobox schon in Neovim eingebaut ist!
 
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "retrobox",
+    callback = function()
+      local hl_groups = { "Normal", "NormalNC", "LineNr", "SignColumn", "EndOfBuffer" }
+      for _, group in ipairs(hl_groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+      end
+      vim.api.nvim_set_hl(0, "Comment", {
+        fg = "#d79921",
+        italic = false,
+        bold = false
+      })
+    end,
+  })
+
+  vim.cmd.colorscheme 'retrobox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }

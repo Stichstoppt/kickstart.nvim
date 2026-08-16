d3b2d75d90df1dad53b777cc15673c9f6fb7a10b
Stichstoppt <paul.stych22@gmail.com>
2026-08-16 23:35:06 +0200

    set copilot to being disabled by default and set keybind to toggle it

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,10 @@
+vim.g.copilot_enabled = false
+
 vim.pack.add {
   'https://github.com/github/copilot.vim',
 }
+
+vim.keymap.set('n', '<leader>cp', function()
+  vim.g.copilot_enabled = not vim.g.copilot_enabled
+  vim.cmd 'Copilot status'
+end, { desc = 'Toggle Copilot' })
